59a92869dfc44b5c9ec28edb2b8e334d5d016883
Marek Marecki <marekjm@ozro.pw>
2014-04-06 14:37:53 +0200

    Added basic tests for Muspyche (partials are not implemented yet)

---
+++ tests/atoms/nested_section.test/run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import difflib
+import json
+import os
+
+import muspyche
+
+# td -- test directory
+td = os.path.join(os.path.split(__file__)[0])
+
+expected = muspyche.util.read(os.path.join(td, 'expected.txt'))
+
+context = json.loads(muspyche.util.read(os.path.join(td, 'context.json')))
+template = muspyche.util.read(os.path.join(td, 'template.mustache'))
+
+actual = muspyche.make(template, context)
+
+
+[print(line) for line in difflib.unified_diff(expected.splitlines(), actual.splitlines(), fromfile='expected', tofile='actual')]
+
+# reverts boolean comparison: yields 1 if not equal, and 0 if equal
+# used to return exit code to console
+exit(not int(actual == expected))

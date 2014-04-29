148a6b2364875585b78ab144cf6cdffb58fa44b7
Marek Marecki <marekjm@ozro.pw>
2014-04-29 11:13:58 +0200

    Added some unittests for context stack implementation

---
+++ tests/contextstack.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+"""Tests for context stack implementation.
+"""
+
+import unittest
+
+import muspyche
+
+
+class ContextStackTests(unittest.TestCase):
+    def testGettingSimpleKeys(self):
+        context = {'foo': 'bar'}
+        stack = muspyche.context.ContextStack(context)
+        self.assertEqual('bar', stack.get('foo'))
+
+    def testGetingNonexistetKeysYieldsEmptyString(self):
+        context = {}
+        stack = muspyche.context.ContextStack(context)
+        self.assertEqual('', stack.get('foo'))
+
+    def testGettingValuesFromNestedDicts(self):
+        context = {'a': {'b': {'c': {'d': '---'}}}}
+        stack = muspyche.context.ContextStack(context)
+        self.assertEqual('---', stack.get('a.b.c.d'))
+
+    def testGettingValuesFromAdjustedContext(self):
+        context = {'a': {'b': {'c': {'d': '---'}}}}
+        stack = muspyche.context.ContextStack(context)
+        self.assertEqual('---', stack.adjust('a.b.c').get('d'))
+
+    def testGettingValuesFromFullyAdjustedContextUsingSingleDotKey(self):
+        context = {'a': {'b': {'c': {'d': '---'}}}}
+        stack = muspyche.context.ContextStack(context)
+        self.assertEqual('---', stack.adjust('a.b.c.d').get('.'))
+
+    def testRestoring(self):
+        context = {'a': {
+                         'b': {
+                               'c': {
+                                     'd': {
+                                           'e': 'E'
+                                           },
+                                     'f': 'F'
+                               },
+                               'g': 'G',
+                               'h': {
+                                     'i': 'I'
+                               }
+                         }
+                   },
+                   'z': 'Z'
+                  }
+        stack = muspyche.context.ContextStack(context)
+        stack.adjust('a.b.c')
+        stack.adjust('d')
+        self.assertEqual('', stack.get('f'))
+        self.assertEqual('E', stack.get('e'))
+        stack.restore()
+        self.assertEqual('F', stack.get('f'))
+        self.assertEqual('', stack.get('e'))
+        stack.adjust('::a.b.h')
+        self.assertEqual('', stack.get('g'))
+        self.assertEqual('I', stack.get('i'))
+        stack.restore()
+        self.assertEqual('F', stack.get('f'))
+        self.assertEqual('', stack.get('i'))
+
+    def testGettingAbsoluteKeysDoesNotInfluenceAdjustmentWhenLocked(self):
+        context = {'home': '/', 'user': {'home': '~'}}
+        stack = muspyche.context.ContextStack(context)
+        stack.adjust('user')
+        self.assertEqual('~', stack.get('home'))
+        stack.lock()
+        self.assertEqual('/', stack.get('::home'))
+        self.assertEqual('~', stack.get('home'))
+
+
+if __name__ == '__main__':
+    unittest.main()
